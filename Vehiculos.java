6d2a3bf0a5f5180f5e771bcf46ee8ce61c07207a
alvaro-ruiz <aaruizblanquez@gmail.com>
2023-02-03 13:23:48 +0100

    Primera clase

---
+++ Vehiculos.java
@@ -0,0 +1,13 @@
+
+public class Vehiculos {
+
+  private String marca;
+  private String modelo;
+  private String matricula;
+  
+  public Vehiculos(String marca, String modelo, String matricula) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.matricula = matricula;
+  }
+}
